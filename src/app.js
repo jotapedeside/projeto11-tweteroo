9b91dae0a35ab92188168e13ff10395393ba0fb7
C <jotapedeside@gmail.com>
2022-08-27 23:11:00 -0300

    feat(/tweets routes): get and post routes added

--- src/app.js
+++ src/app.js
@@ -18,19 +18,62 @@ let tweets = [];
 
 app.post('/sign-up', (req, res) => {
   const data = req.body;
-  console.log(data);
-  console.log(data.username.length);
   if (data.username.length === 0 || data.avatar.length === 0) {
-    //res.status(400).json({status: 400, message: "Todos os campos são obrigatórios"});
-    res.status(400).send("Todos os campos são obrigatórios");
+    res.sendStatus(400).json({status: 400, message: "Todos os campos são obrigatórios"});
   } else {
     users.push(data);
-    //res.status(201).json({ status: 201, message: "OK" });
-    res.status(201).send("OK");
+    res.status(201).json({ status: 201, message: "OK" });
   }
 });
 
+app.post('/tweets', (req, res) => {
 
+  /*const profPic = users.find(e => e.username === req.body.username).avatar;
+  const avatar = profPic ? profPic : "";
+  tweets.push(newTweet);
+  res.send(newTweet);
+  const user = req.headers.user;
+  const profPic = users.find(u => u.username === user);
+  console.log("link da prof pic", profPic);
+  console.log("find res", users.find(u => u.username === user));
+  console.log("find res2", users.find(u => u.username === user).avatar);
+  const avatar = profPic ? profPic.avatar : "oi";
+
+  
+  if (req.body.tweet.length !== 0 || profPic !== 0) {
+    const newTweet = {
+      ...req.body,
+      asd,
+    }
+    tweets.push(newTweet)
+    res.send(newTweet)
+  } else {
+    res.sendStatus(400).json({status: 400, message: "Todos os campos são obrigatórios"});
+  }*/
+  const profPic = users.find(user => user.username === req.body.username)
+  
+  if (req.body.tweet.length !== 0 || profPic !== 0) {
+    const newTweet = {
+      ...req.body,
+      avatar: profPic.avatar,
+    }
+    tweets.push(newTweet)
+    res.status(201).send(newTweet).json({ status: 201, message: "OK" })
+  } else {
+    res.sendStatus(400).json({status: 400, message: "Todos os campos são obrigatórios"});
+  }
+
+
+});
+
+app.get('/tweets', (req, res) => {
+  console.log(req);
+  const latestTweets = [];
+  for (let i = 0; i < tweets.length; i++) {
+    latestTweets.push(tweets[i]);
+  }
+  res.status(200).send(latestTweets);
+});
 
 
 
