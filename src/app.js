902f0a912a84f4c6c07bc9dba421a1a42011d666
C <jotapedeside@gmail.com>
2022-08-28 14:31:29 -0300

    feature(post routes): post routes created

--- src/app.js
+++ src/app.js
@@ -9,48 +9,39 @@ app.use(cors());
 let users = [];
 let tweets = [];
 
-/*app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  next();
-});*/
-
 app.post('/sign-up', (req, res) => {
   const data = req.body;
-  if (data.username.length === 0 || data.avatar.length === 0) {
-    res.sendStatus(400).json({status: 400, message: "Todos os campos são obrigatórios"});
-  } else {
+  if (data.username.length !== 0 || data.avatar.length !== 0) {
     users.push(data);
-    res.status(201).json({ status: 201, message: "OK" });
+    res.status(201).send("OK").json({ status: 201, message: "OK" });
+  } else {
+    res.sendStatus(400).json({status: 400, message: "Todos os campos são obrigatórios"});
   }
 });
 
 app.post('/tweets', (req, res) => {
-  const profPic = users.find(user => user.username === req.body.username)
+  const profPic = users.find(user => user.username === req.body.username);
   
   if (req.body.tweet.length !== 0 || profPic !== 0) {
     const newTweet = {
       ...req.body,
-      avatar: profPic.avatar,
+      avatar: profPic.avatar
     }
     tweets.push(newTweet)
-    res.status(201).send(newTweet).json({ status: 201, message: "OK" })
+    res.status(201).send(newTweet).json({ status: 201, message: "OK" });
   } else {
     res.sendStatus(400).json({status: 400, message: "Todos os campos são obrigatórios"});
   }
 });
 
 app.get('/tweets', (req, res) => {
-  console.log(req);
-  const latestTweets = [];
-  for (let i = 0; i < tweets.length; i++) {
-    latestTweets.push(tweets[i]);
-  }
-  res.status(200).send(latestTweets);
+    console.log(req);
+    const latestTweets = [];
+    for (let i = 0; i < tweets.length; i++) {
+      latestTweets.push(tweets[i]);
+    }
+    res.status(200).send(latestTweets);
 });
-
-
 
 app.listen(5000,
   () => {console.log("Magic happens at 5000")
